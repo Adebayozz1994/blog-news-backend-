afec388272b5043ad9768685be528500c7c02297
Ogunlade Adebayo Peter <144332360+Adebayozz1994@users.noreply.github.com>
2024-09-08 16:56:07 -0100

    new commit

--- app/Http/Controllers/Auth/AuthenticatedSessionController.php
+++ app/Http/Controllers/Auth/AuthenticatedSessionController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers\Auth;
 
 use App\Http\Controllers\Controller;
 use App\Http\Requests\Auth\LoginRequest;
+use App\Models\User;
 use Illuminate\Http\RedirectResponse;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
@@ -19,22 +20,48 @@ class AuthenticatedSessionController extends Controller
         return view('auth.login');
     }
 
+    // public function getUser(Request $request){
+    //     $id = substr($request->token, -1);
+    //         $user = User::where('id', $id)->first();
+    //       if($request->token === $user->token){
+    //         return response()->json([
+    //           'status' => true,
+    //           'admin' => $user
+    //         ]);
+    //       } else{
+    //         return response()->json([
+    //           'status' => false,
+    //           'message' => 'Please login again'
+    //         ]);
+    //       }
+        
+    //   }
     /**
      * Handle an incoming authentication request.
      */
-    public function store(LoginRequest $request): RedirectResponse
+    public function store(LoginRequest $request)
     {
-        $request->authenticate();
-
-        $request->session()->regenerate();
-
-        return redirect()->intended(route('dashboard', absolute: false));
+        $credentials = $request->only('email', 'password');
+    
+        if (Auth::attempt($credentials)) {
+            $request->session()->regenerate();
+            
+            return response()->json([
+                'status' => true,
+                'user' => Auth::user(),
+            ]);
+        }
+    
+        return response()->json([
+            'status' => false,
+            'error' => 'The provided credentials do not match our records.',
+        ]);
     }
 
     /**
      * Destroy an authenticated session.
      */
-    public function destroy(Request $request): RedirectResponse
+    public function destroy(Request $request)
     {
         Auth::guard('web')->logout();
 
@@ -42,6 +69,12 @@ class AuthenticatedSessionController extends Controller
 
         $request->session()->regenerateToken();
 
-        return redirect('/');
+        // return redirect('/');
+           // Optionally return success message
+           return response()->json([
+            'status' => true,
+            'message' => 'User logged out successfully.',
+        ]);
+     
     }
 }
